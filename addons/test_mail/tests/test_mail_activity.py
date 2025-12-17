96c1f77b8fb38843150d3b60622a1cb81c2e8449
Xavier-Do <xdo@odoo.com>
2025-12-17 14:16:30 +0100

    [REF] test_mail: move tests post install
    Some tests where running at install by default for historical reasons,
    this commit moves them post install to speedup testing time.
    
    Part-of: odoo/odoo#240404
    Related: odoo/enterprise#102430
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

--- addons/test_mail/tests/test_mail_activity.py
+++ addons/test_mail/tests/test_mail_activity.py
@@ -29,7 +29,6 @@ class TestActivityCommon(ActivityScheduleCase):
 
 
 @tests.tagged('mail_activity')
-@tagged('at_install', '-post_install')  # LEGACY at_install
 class TestActivityRights(TestActivityCommon):
 
     def test_activity_action_open_document_no_access(self):
@@ -260,7 +259,6 @@ class TestActivityRights(TestActivityCommon):
 
 
 @tests.tagged('mail_activity')
-@tagged('at_install', '-post_install')  # LEGACY at_install
 class TestActivityFlow(TestActivityCommon):
 
     def test_activity_flow_employee(self):
@@ -644,7 +642,6 @@ class TestActivitySystray(TestActivityCommon, HttpCase):
 
 @tests.tagged('mail_activity')
 @freeze_time("2024-01-01 09:00:00")
-@tagged('at_install', '-post_install')  # LEGACY at_install
 class TestActivitySystrayBusNotify(TestActivityCommon):
 
     @classmethod
@@ -766,7 +763,6 @@ class TestActivitySystrayBusNotify(TestActivityCommon):
 
 
 @tests.tagged('mail_activity')
-@tagged('at_install', '-post_install')  # LEGACY at_install
 class TestActivityViewHelpers(TestActivityCommon):
 
     @classmethod
